4909be3e942e4e5c2a2f627ce359db56ad774cb7
xuerongNanopay <xuerong@nanopay.net>
2024-12-11 00:39:05 -0500

    add help method for directory

--- src/sequential_io.rs
+++ src/sequential_io.rs
@@ -1,15 +1,41 @@
 use std::path::Path;
+use std::fs;
 
 fn is_directory_exist(path: &str) -> bool {
     return Path::new(path).exists()
+}
+
+fn rm_directory(path: &str) {
+    if is_directory_exist(path) {
+        fs::remove_dir_all(path).unwrap()
+    }
+}
+
+fn create_directory(path: &str) {
+    if !is_directory_exist(path) {
+        fs::create_dir(path).unwrap()
+    } else {
+        fs::remove_dir_all(path).unwrap();
+        fs::create_dir(path).unwrap()
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const TMP_DIR: &str = "./tmp";
+
+    fn initial_dir() {
+        create_directory(TMP_DIR)
+    }
+
+    fn clear_dir() {
+        rm_directory(TMP_DIR)
+    }
+
     #[test]
     fn sequential_io() {
-        println!("{}", is_directory_exist("./target"));
+        create_directory(TMP_DIR)
     }
 }
